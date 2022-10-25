85914a0391a779a015388a922ef81627ae53c354
a.atabiev <endlessspring.of@gmail.com>
2022-10-25 21:22:12 +0300

    clear locale

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,14 +1,3 @@
 export default {
-    "common.today": 'Today',
-    "common.inbox": 'Inbox',
-    "common.anytime": 'Anytime',
-    "common.sometime": 'Sometime',
-    "common.settings": 'Settings',
-    "common.deleteList": 'Delete list',
-    "common.duplicateList": 'Duplicate list',
-
-    "sidebar.addList": 'Add list',
-
-    "lang.ru": "Russian",
-    "lang.en": "English",
+ 
 }
--- src/i18n/ru.ts
+++ src/i18n/ru.ts
@@ -1,15 +1,3 @@
 export default {
-    "common.settings": 'Настройки',
-    "common.today": 'Сегодня',
-    "common.inbox": 'Входящие',
-    "common.anytime": 'В любое время',
-    "common.sometime": 'Когда-нибудь',
-    "common.deleteList": 'Удалить список',
-    "common.duplicateList": 'Дублировать список',
 
-
-    "sidebar.addList": 'Добавить список',
-
-    "lang.ru": 'Русский',
-    "lang.en": 'Английский',
 }
